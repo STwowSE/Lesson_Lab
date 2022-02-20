bc56f4ab501d3592535f9a9efd3f55321eece3d9
STwowSE <99324237+STwowSE@users.noreply.github.com>
2022-02-20 12:46:59 +0300

    Add files via upload

---
+++ Новая папка/L1Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_1Lab
+{
+    internal class L1Task
+    {
+        /*static void Main()
+        {
+            char g = 'm';
+            string n = "Ivan";
+            string nn = "Ivanov";
+            string y = "1993.1.2";
+            string a = "Rostov-on-don";
+            string c = "Rassia";
+            string nc = "CS211";
+            int p = 56;
+            int w = 16;
+            string pr = "Mayer Svetlena";
+            Console.WriteLine("Информация о студенте:\n" +
+                "Имя: " + n + ", " + "Фамиилия: " + nn + ", " +
+                "Дата рождения: " + y + ",\n" +
+                "Адрес: " + a + ", " + "Пол: " + g + ", "+
+                "Страна: " + c + ",\n"+
+                "Course Information:\n" +
+                "Название кураса: " + nc + ", " + 
+                "Баллы: " + p + ", " + "Количество занятий в неделю: "+
+                w + ", " + "Преподаватель: " + pr);
+            Console.ReadKey();
+        }*/
+    }
+}
